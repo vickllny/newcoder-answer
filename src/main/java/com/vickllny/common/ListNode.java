99d85f88f2949385d08c9c00dcf559362311c2df
vickllny <vickllny@163.com>
2023-11-07 22:37:24 +0800

    BM13  不在状态

---
+++ src/main/java/com/vickllny/common/ListNode.java
@@ -0,0 +1,10 @@
+package com.vickllny.common;
+
+public class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    public ListNode(int val){
+        this.val = val;
+    }
+}
--- src/main/java/com/vickllny/linkedlist/BM13.java
+++ src/main/java/com/vickllny/linkedlist/BM13.java
@@ -1,5 +1,76 @@
 package com.vickllny.linkedlist;
 
+import com.vickllny.common.ListNode;
+
 public class BM13 {
     
+    public static boolean isPail(ListNode head){
+        if(head == null){
+            return false;
+        }
+        if(head.next == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (true) {
+            if((fast.next) == null){
+                //偶数
+                fast = slow.next;
+                slow = head;
+                break;
+            }
+            fast = fast.next;
+            if((fast.next) == null){
+                //奇数
+                fast = slow.next.next;
+                slow = head;
+                break;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        fast = reverse(fast);
+        //判断是否相等
+        while (fast != null) {
+            if(slow.val != fast.val){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    static ListNode reverse(ListNode root){
+        ListNode prev = null;
+        while (root != null) {
+            ListNode tempNode = root.next;
+            root.next = prev;
+            prev = root;
+            root = tempNode;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode root = buildListNode(new int[]{-1,-2,-3,-2,-1});
+        System.out.println(isPail(root));
+    }
+
+    static ListNode buildListNode(int[] number){
+        if(number == null || number.length == 0){
+            return null;
+        }
+        if(number.length == 1){
+            return new ListNode(number[0]);
+        }
+        ListNode root = new ListNode(number[0]);
+        ListNode tempNode = root;
+        for(int i = 1; i < number.length; i++){
+            tempNode.next = new ListNode(number[i]);
+            tempNode = tempNode.next;
+        }
+        return root;
+    }
 }
